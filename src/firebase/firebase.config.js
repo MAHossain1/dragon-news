785316c8719a47615c52f2ab70f7aa19f6e45a44
Md. Arman Hossain <mahpro110@gmail.com>
2023-02-18 23:34:26 +0600

    started dragon news site build

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBrbb-n2H7VnMie_XOdIzXb9MCwo-16v4A",
+  authDomain: "dragon-news-4e333.firebaseapp.com",
+  projectId: "dragon-news-4e333",
+  storageBucket: "dragon-news-4e333.appspot.com",
+  messagingSenderId: "490279921242",
+  appId: "1:490279921242:web:a3f98e39e3bb96a1af5a63",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
