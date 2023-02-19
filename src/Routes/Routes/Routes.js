217ae8575b233d61605fdced4162e061c9126d1a
Md. Arman Hossain <mahpro110@gmail.com>
2023-02-19 06:31:49 +0600

    folder structure done

--- src/Routes/Routes/Routes.js
+++ src/Routes/Routes/Routes.js
@@ -1,11 +1,27 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../../layouts/Main";
+import Category from "../../pages/Category/Category";
+import Home from "../../pages/Home/Home";
+import News from "../../pages/News/News";
 
 const routes = createBrowserRouter([
   {
     path: "/",
     element: <Main></Main>,
-    children: [{}],
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+      {
+        path: "/category/:id",
+        element: <Category></Category>,
+      },
+      {
+        path: "/news/:id",
+        element: <News></News>,
+      },
+    ],
   },
 ]);
 
